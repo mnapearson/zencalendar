85aaa6da88138e6343684c36de079dcad8853a6a
mnapearson <marratoon@gmail.com>
2020-09-25 23:02:32 +0200

    add cube to zencal

--- frontend/src/views/Calendar.vue
+++ frontend/src/views/Calendar.vue
@@ -4,8 +4,70 @@
       href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet"
     />
-    <router-link class="create" to="/create">Create</router-link>
-    <button @click="logout">logout</button>
+    <nav>
+      <div class="scene">
+        <a href="https://celestial-weather.netlify.app">
+          <div class="cube">
+            <div class="cube-face cube-face-front">
+              <img
+                src="https://img2.pngio.com/test-your-knowledge-5-little-known-facts-about-the-sistine-chapel-sistine-chapel-png-975_648.png"
+                alt="sistine-chapel"
+                height="50px"
+                width="50px"
+                id="cube-image"
+              />
+            </div>
+            <div class="cube-face cube-face-back">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Venus_botticelli_detail.jpg/1920px-Venus_botticelli_detail.jpg"
+                alt="face of venus"
+                height="50px"
+                width="50px;"
+                id="cube-image"
+              />
+            </div>
+            <div class="cube-face cube-face-left">
+              <img
+                src="https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvYXNzZXRzLzIxZjliM2QwLTc5Y2QtNGFmMS05NDc4LTMwZjBjYjkzNmMwNGJmYTVjNmI5M2NjMjgwNDg0YV9Gb3JiaWRkZW5fZnJ1aXQuanBnIl0sWyJwIiwiY29udmVydCIsIi1hdXRvLW9yaWVudCAiXSxbInAiLCJ0aHVtYiIsIjExNTJ4NzY4Kzc3KzIiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il0sWyJwIiwidGh1bWIiLCI2MDB4PiJdXQ/Forbidden_fruit.jpg"
+                alt="adam and eve"
+                height="50px"
+                width="50px;"
+                id="cube-image"
+              />
+            </div>
+            <div class="cube-face cube-face-right">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Michelangelo%2C_giudizio_universale%2C_dettagli_33.jpg/330px-Michelangelo%2C_giudizio_universale%2C_dettagli_33.jpg"
+                alt="last judgment"
+                height="50px"
+                width="50px;"
+                id="cube-image"
+              />
+            </div>
+            <div class="cube-face cube-face-top">
+              <img
+                src="https://i.pinimg.com/originals/05/14/7d/05147d70df9f77b36aa897bde585826c.jpg"
+                alt="sun and moon"
+                height="50px"
+                width="50px;"
+                id="cube-image"
+              />
+            </div>
+            <div class="cube-face cube-face-bottom">
+              <img
+                src="https://i.pinimg.com/originals/ee/ca/b5/eecab574e99361f679c12dd6bbfb3aa8.jpg"
+                alt="the flood"
+                height="50px"
+                width="50px;"
+                id="cube-image"
+              />
+            </div>
+          </div>
+        </a>
+      </div>
+      <router-link class="create" to="/create">Create</router-link>
+      <button @click="logout">logout</button>
+    </nav>
     <h1>Calendar</h1>
     <div class="calendar">
       <SingleDatePicker @selectDate="selectDate" />
@@ -97,6 +159,101 @@ export default {
 </script>
 
 <style scoped>
+nav {
+  margin: 1rem;
+  display: flex;
+  justify-content: space-between;
+}
+
+.scene {
+  margin-top: 1rem;
+
+  display: flex;
+  width: 25px;
+  height: 25px;
+  perspective: 2400px;
+}
+
+.cube {
+  width: 50px;
+  height: 50px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateX(30deg) rotateY(30deg);
+  -webkit-transform-style: preserve-3d;
+  -webkit-animation: rotatecube 40s infinite;
+
+  -moz-transform-style: preserve-3d;
+  -moz-animation: rotatecube 40s infinite;
+}
+
+@-webkit-keyframes rotatecube {
+  0% {
+    -webkit-transform: rotateX(0deg) rotateY(360deg) rotateZ(90deg);
+  }
+  25% {
+    -webkit-transform: rotateX(90deg) rotateY(270deg) rotateZ(180deg);
+  }
+  50% {
+    -webkit-transform: rotateX(180deg) rotateY(180deg) rotateZ(0deg);
+  }
+  75% {
+    -webkit-transform: rotateX(270deg) rotateY(90deg) rotateZ(360deg);
+  }
+  100% {
+    -webkit-transform: rotateX(360deg) rotateY(0deg) rotateZ(270deg);
+  }
+}
+
+@-moz-keyframes rotatecube {
+  0% {
+    -moz-transform: rotateX(0deg) rotateY(360deg) rotateZ(90deg);
+  }
+  25% {
+    -moz-transform: rotateX(90deg) rotateY(270deg) rotateZ(180deg);
+  }
+  50% {
+    -moz-transform: rotateX(180deg) rotateY(180deg) rotateZ(0deg);
+  }
+  75% {
+    -moz-transform: rotateX(270deg) rotateY(90deg) rotateZ(360deg);
+  }
+  100% {
+    -moz-transform: rotateX(360deg) rotateY(0deg) rotateZ(270deg);
+  }
+}
+
+.cube-face {
+  width: inherit;
+  height: inherit;
+  position: absolute;
+  opacity: 0.7;
+}
+
+.cube-face-front {
+  transform: translate3d(0, 0, 25px);
+}
+
+.cube-face-back {
+  transform: rotateY(180deg) translate3d(0, 0, 25px);
+}
+
+.cube-face-left {
+  transform: rotateY(-90deg) translate3d(0, 0, 25px);
+}
+
+.cube-face-right {
+  transform: rotateY(90deg) translate3d(0, 0, 25px);
+}
+
+.cube-face-top {
+  transform: rotateX(90deg) translate3d(0, 0, 25px);
+}
+
+.cube-face-bottom {
+  transform: rotateX(-90deg) translate3d(0, 0, 25px);
+}
+
 * {
   margin: 0 auto;
   max-width: 750px;
